89af7e5e1082c2710b094e37180c8210d15b1e13
F3nix <shivansh.rawat@enine.school>
2024-04-11 19:27:39 +0530

    Modified Scraper to download and save data day-wise for specific dates

--- README.md
+++ README.md
@@ -16,11 +16,11 @@ The function ```scrape(ip_list):``` accepts a list of dictionaries as input, the
         "format": "csv",
         "download": True
     },
-    "filter": [
-        {
-            "datetime_beginning_ept": "01/26/2024 00:00 to 01/30/2024"
-        }
-    ]
+    "dates": {
+        "date_column":"datetime_beginning_ept", 
+        "date_start":"11/08/2022", 
+        "date_end":"11/10/2022"
+    }
 }
 ```
 Here 
@@ -30,38 +30,13 @@ Here
 * params is the parameters to be passed
     * rowCount is the maximum number of rows that should be in the downloaded file 1000000 is the maximum limit any value above that will lead to a status code 400 error
     * startRow specifies the one-based number of the first record in the search results to be returned. This is one-based, so 1 is the smallest value allowed and will return the very first record
-* Filter contains oarameters based on which the data should be filtered
-    * Date Time Attributes are used to get the report for a specific time period
+* Dates contains attributes related to date, the data will be downloaded only for the date range specified here, this attribute is mandatory
+    * date_column: The column name from which the dates should be considered
+    * date_start: The start date
+    *date_end: The end date
 
 #### Following params can also be added
 * sort: Specifies the name of the column to sort on
 * order: Specifies the direction to sort the field on. Allowed values are: "asc" for ascending or "desc" for
 descending order. The default value is "asc".
 * fields: Specifies the name of the columns to be included in the file, by default all columns are included
-
-
-#### Date Time Attributes can be entered in the following formats:
-* Today (returns data for today's day) 
-* Yesterday (returns data for today's date -1 day)
-* LastWeek (returns data where the date falls within the last calendar week)
-* LastMonth (returns data where the date falls within the last calendar month)
-* CurrentMonth (returns data where the date value falls within the current month including current day)
-* CurrentWeek (returns data where the date value falls within the current week including current day)
-* CurrentYear (returns data where the date value falls within the current calendar year including current day)
-* LastYear (returns data where the date value falls within the last calendar year)
-* NextYear (returns data where the date value falls within the next calendar year)
-* NextMonth (returns data where the date value falls within the next calendar month)
-* NextWeek (returns data where the date value falls within the next calendar week)
-* Tomorrow (returns data for current calendar day +1 day) 
-* CurrentHour (returns data where the date value is equal to today’s date and the time is equal to the current hour)
-* 1MonthAgo (returns data for the month where the date value falls within the current full calendar month minus one)
-* 4MonthsAgo (returns data for the month where the date value falls within the current full calendar month minus four)
-* 6MonthsAgo (returns data for the month where the date value falls within the current full calendar month minus six)
-* LastHour (returns data where the date value is equal to today’s date and the time is equal to the current hour-1 hour)
-* 15SecondsAgo (returns data where timestamp value is equal to current timestamp minus 15 seconds)
-* 5MinutesAgo (returns data where timestamp value is equal to current timestamp minus 5 seconds)
-* [date-value] to [date-value] The date-value should include the date and time component or the date component for applicable feeds. The date-value range should be within 366 days. <BR> **Example:** yyyy-mm-dd hh:mi to yyyy-mm-dd hh:mi
-
-15SecondsAgo and 5MinutesAgo are designed specifically for dispatch rates and unverified LMP feeds
-that update every 15 seconds and 5 minutes respectively, additionally these filters are designed to work
-only with the ept columns, do not use with the utc columns.
--- scraper.py
+++ scraper.py
@@ -1,11 +1,16 @@
-import time
+import os
 import requests
+from datetime import datetime, timedelta
+
+
+def save_file(path, file):
+    with open(path, "wb") as f:
+        f.write(file.content)
+    print(f"{path} file saved successfully!")
 
 
 def scrape(ip_list):
-    headers = {
-        "Ocp-Apim-Subscription-Key": "18b56f8b0eda44efabe5d60a5270cc34",
-    }
+    headers = {"Ocp-Apim-Subscription-Key": "18b56f8b0eda44efabe5d60a5270cc34"}
     title_map = {}
     for ip_dict in ip_list:
         if ip_dict["iso"] not in title_map:
@@ -13,35 +18,68 @@ def scrape(ip_list):
             data = requests.get(ip_dict["url"], headers=headers, timeout=900)
             for i in data.json()["items"]:
                 title_map[ip_dict["iso"]][i["displayName"]] = i["name"]
-
-    timestamp = time.mktime(time.localtime())
-    counter = 0
-    for ip_dict in ip_list:
         name = title_map[ip_dict["iso"]][ip_dict["title"]]
-        csv_url = f"{ip_dict['url']}/{name}"
-        default_params = requests.get(csv_url, headers=headers, timeout=900)
-        params = ip_dict["params"]
-        if "filter" in ip_dict:
-            for i in ip_dict["filter"]:
-                params.update(i)
-        else:
-            for i in default_params.json()["searchSpecification"]["filters"]:
-                params.update(i)
-        response = requests.get(csv_url, params=params, headers=headers, timeout=900)
-        if response.status_code == 200:
-            with open(name + ".csv", "wb") as f:
-                f.write(response.content)
-            print(f"{name}.csv file saved successfully!")
-        else:
-            print(
-                f"Failed to fetch data for {ip_dict['title']}. Status code:",
-                response.status_code,
-            )
 
-        counter += 1
-        if counter == 6:
-            counter = 0
-            diff = 60 - (time.mktime(time.localtime()) - timestamp)
-            if diff > 0:
-                time.sleep(diff)
-            timestamp = time.mktime(time.localtime())
+        csv_url = f"{ip_dict['url']}/{name}"
+        params = ip_dict["params"]
+        dates = ip_dict["dates"]
+
+        start_date = datetime.strptime(dates["date_start"], "%m/%d/%Y").date()
+        end_date = datetime.strptime(dates["date_end"], "%m/%d/%Y").date()
+        date_range = []
+        current_date = start_date
+        while current_date <= end_date:
+            date_range.append(current_date)
+            current_date += timedelta(days=1)
+
+        for date in date_range:
+            params.update({dates["date_column"]: date.strftime("%Y-%m-%d")})
+            year = date.year
+            month = date.month
+            day = date.day
+            print(params)
+            response = requests.get(
+                csv_url, params=params, headers=headers, timeout=900
+            )
+            if response.status_code == 200:
+                directory_path = f"{ip_dict['title']}/{year}/{month}/{day}/"
+                os.makedirs(directory_path, exist_ok=True)
+                if int(response.headers["X-TotalRows"]) > params["rowCount"]:
+                    counter = 1
+                    while params["startRow"] < int(response.headers["X-TotalRows"]):
+                        save_file(f"{directory_path}{name}{counter}.csv", response)
+                        params["startRow"] += params["rowCount"]
+                        response = requests.get(
+                            csv_url, params=params, headers=headers, timeout=900
+                        )
+                        counter += 1
+                    params["startRow"] = 1
+                else:
+                    save_file(f"{directory_path}{name}.csv", response)
+            else:
+                print(
+                    f"Failed to fetch data for file. Status code:",
+                    response.status_code,
+                )
+
+
+scrape(
+    [
+        {
+            "iso": "PJM",
+            "url": "https://api.pjm.com/api/v1/",
+            "title": "Day-Ahead Hourly LMPs",
+            "params": {
+                "rowCount": 1000000,
+                "startRow": 1,
+                "format": "csv",
+                "download": True,
+            },
+            "dates": {
+                "date_column": "datetime_beginning_ept",
+                "date_start": "11/08/2022",
+                "date_end": "11/10/2022",
+            },
+        },
+    ]
+)
