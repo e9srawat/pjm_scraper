fdaa05e2eb03f599262f044803bddf61f49b7e71
F3nix <shivansh.rawat@enine.school>
2024-04-17 18:21:32 +0530

    Changed the way the csv files were being written

--- scraper.py
+++ scraper.py
@@ -1,12 +1,17 @@
 import os
+import gzip
+import tablib
 import time
 import requests
 from datetime import datetime, timedelta
 
+HEADERS = {"Ocp-Apim-Subscription-Key": "18b56f8b0eda44efabe5d60a5270cc34"}
 
 def save_file(path, file):
-    with open(path, "wb") as f:
-        f.write(file.content)
+    content = file.content.decode('utf-8')
+    dataset = tablib.Dataset().load(content, format='csv')
+    with gzip.open(path + '.gz', "wb") as f:
+        f.write(dataset.export('csv').encode('utf-8'))
     print(f"{path} file saved successfully!")
     
 def timeout(timestamp):
@@ -17,32 +22,19 @@ def timeout(timestamp):
         time.sleep(diff)
     return time.mktime(time.localtime())
 
+def get_name(url, title):
+    data = requests.get(url, headers=HEADERS, timeout=900, stream=False)
+    for i in data.json()["items"]:
+        if i["displayName"] == title:
+            return i["name"]
 
 def scrape(ip_list):
-    headers = {"Ocp-Apim-Subscription-Key": "18b56f8b0eda44efabe5d60a5270cc34"}
-    title_map = {}
-    
-    t = time.localtime()
-    timestamp = time.mktime(t)
-    counter = 0
+    requests.models.CONTENT_CHUNK_SIZE = 50 * 1024 * 1024
     for ip_dict in ip_list:
-        if ip_dict["iso"] not in title_map:
-            title_map[ip_dict["iso"]] = {}
-            
-            if counter>=6:
-                timestamp = timeout(timestamp)
-                counter = 0 
-            data = requests.get(ip_dict["url"], headers=headers, timeout=900)
-            counter+=1
-            
-            for i in data.json()["items"]:
-                title_map[ip_dict["iso"]][i["displayName"]] = i["name"]
-        name = title_map[ip_dict["iso"]][ip_dict["title"]]
-
+        name = get_name(ip_dict["url"],ip_dict["title"])
         csv_url = f"{ip_dict['url']}/{name}"
         params = ip_dict["params"]
         dates = ip_dict["dates"]
-
         start_date = datetime.strptime(dates["date_start"], "%m/%d/%Y").date()
         end_date = datetime.strptime(dates["date_end"], "%m/%d/%Y").date()
         date_range = []
@@ -50,64 +42,36 @@ def scrape(ip_list):
         while current_date <= end_date:
             date_range.append(current_date)
             current_date += timedelta(days=1)
-
         for date in date_range:
             params.update({dates["date_column"]: date.strftime("%Y-%m-%d")})
             year = date.year
             month = date.month
             day = date.day
-            
-            if counter>=6:
-                timestamp = timeout(timestamp)
-                counter = 0 
             response = requests.get(
-                csv_url, params=params, headers=headers, timeout=900
+                csv_url, params=params, headers=HEADERS, timeout=900, stream=False
             )
-            counter+=1
-            
-            if response.status_code == 200:
-                directory_path = f"{ip_dict['title']}/{year}/{month}/{day}/"
-                os.makedirs(directory_path, exist_ok=True)
+            if response.status_code == 200: 
                 if int(response.headers["X-TotalRows"]) > params["rowCount"]:
+                    directory_path = f"{ip_dict['title']}/{year}/{month}/{day}/"
+                    os.makedirs(directory_path, exist_ok=True)
                     part = 1
                     while params["startRow"] < int(response.headers["X-TotalRows"]):
                         save_file(f"{directory_path}{name}{part}.csv", response)
                         params["startRow"] += params["rowCount"]
-                        if counter>=6:
-                            timestamp = timeout(timestamp)
-                            counter = 0 
                         response = requests.get(
-                            csv_url, params=params, headers=headers, timeout=900
+                            csv_url, params=params, headers=HEADERS, timeout=900, stream=False
                         )
-                        counter+=1
                         part += 1
                     params["startRow"] = 1
+                elif int(response.headers["X-TotalRows"]) == 0:
+                    print("skipping blank file")
+                    continue
                 else:
+                    directory_path = f"{ip_dict['title']}/{year}/{month}/{day}/"
+                    os.makedirs(directory_path, exist_ok=True)
                     save_file(f"{directory_path}{name}.csv", response)
             else:
                 print(
                     f"Failed to fetch data for file. Status code:",
                     response.status_code,
                 )
-
-
-scrape(
-    [
-        {
-            "iso": "PJM",
-            "url": "https://api.pjm.com/api/v1/",
-            "title": "PJM Regulation Zone Preliminary Billing Data",
-            "params": {
-                "rowCount": 1000000,
-                "startRow": 1,
-                "format": "csv",
-                "download": True,
-            },
-            "dates": {
-                "date_column": "datetime_beginning_ept",
-                "date_start": "10/1/2012",
-                "date_end": "4/11/2024",
-            },
-        },
-    ]
-)
